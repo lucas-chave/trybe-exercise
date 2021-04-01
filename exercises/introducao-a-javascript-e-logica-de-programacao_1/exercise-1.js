b878234a3e3d4dfe1680660a62c4f528a4dd23e1
lucas-chave <lcchavesr150@gmail.com>
2021-04-01 14:42:47 -0300

    exercise 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise-1.js
@@ -0,0 +1,22 @@
+const a = 5;
+const b = 10;
+
+function sum() {
+  return a + b;
+}
+
+function subtraction() {
+  return a - b;
+}
+
+function division() {
+  return a / b;
+}
+
+function multiplication() {
+  return a * b;
+}
+
+function module() {
+  return a % b;
+}
