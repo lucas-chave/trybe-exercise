055d3d3931211b51b03ec2746de46cc2fb3e75b4
lucas-chave <lcchavesr150@gmail.com>
2021-04-01 16:27:47 -0300

    exercise 6

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise-5.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+function arr() {
+  for(let index = 0; index < numbers.length; index += 1) {
+    return Math.max(numbers[index]) 
+    
+  }
+}
+
+console.log(arr())
