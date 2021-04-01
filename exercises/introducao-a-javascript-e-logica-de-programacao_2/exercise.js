88263474032139faf444876ff218762766d3abe2
lucas-chave <lcchavesr150@gmail.com>
2021-04-01 16:05:32 -0300

    exercise 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise.js
@@ -3,9 +3,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 function sumArray() {
   let total =0;
   for(let index = 0; index < numbers.length; index += 1) {
-    total += numbers[index];
+    total += numbers[index]; 
   }
-  return total;
+  
+  total / numbers.length;
+  
+  if(total > 20) {
+    return 'valor maior que 20';
+  } 
+  return 'valor menor ou igual a 20';
 }
 
 console.log(sumArray());
